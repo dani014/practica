4124d8b98cde6127088c793d7330fa63a97cadd1
dani <dani@dani-laptop.(none)>
2012-12-08 23:22:22 +0000

    devise

---
+++ snap2day/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snap2day::Application.config.secret_token = '75aa547595772fc82b47eee3cdb3583b2e3fd5e0be051e20991d85ee10a799eacae5750b1ada638f82d36e6499600f0affec4e265a128d75ee94ed77871e5b5d'
